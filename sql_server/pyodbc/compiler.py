b23d305eb7596c58eb6c698795473df268f353ac
Diego Magalhães <diego@perflo.co>
2022-05-04 11:06:20 -0300

    Check if explain query prop exists Fixes #125

--- sql_server/pyodbc/compiler.py
+++ sql_server/pyodbc/compiler.py
@@ -291,7 +291,7 @@ class SQLCompiler(compiler.SQLCompiler):
                     result.append('HAVING %s' % having)
                     params.extend(h_params)
 
-            if self.query.explain_query:
+            if hasattr(self, 'query.explain_query'):
                 result.insert(0, self.connection.ops.explain_query_prefix(
                     self.query.explain_format,
                     **self.query.explain_options
